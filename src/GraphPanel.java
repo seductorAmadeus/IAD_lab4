d656a0e64d9d467db390055da0575e70936cb085
amadeus-tentatio <martinraila@mail.ru>
2016-11-27 11:16:52 +0300

    Delivers the panel graph in the  separate class

--- src/GraphPanel.java
+++ src/GraphPanel.java
@@ -1,17 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Graph extends JPanel implements Runnable {
-    private final int DEFAULT_GRAPH_HEIGHT = 450;
-    private final int DEFAULT_GRAPH_WIDTH = 450;
+public class GraphPanel extends JPanel implements Runnable, ActionListener {
+    private final int DEFAULT_GRAPH_HEIGHT = 470;
+    private final int DEFAULT_GRAPH_WIDTH = 470;
     private boolean flag;
     private double x, y;
-    private int radius = 4;
+    private int radius = 5;
     private int step;
     private int green, red;
     private int stepPast = 25;
     private int count = 0;
     private Color colorOfThePlotArea = Color.black;
+
+    GraphPanel() {
+        this.setPreferredSize(new Dimension(470, 260));
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    }
 
     public boolean getFlag() {
         return flag;
@@ -76,27 +83,30 @@ public class Graph extends JPanel implements Runnable {
         graph.fillPolygon(PolygonPoints.X_POINTS, PolygonPoints.Y_POINTS, PolygonPoints.N_POINTS);
 
         graphic.setColor(new Color(0xFF0045));
-        graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2, DEFAULT_GRAPH_HEIGHT, DEFAULT_GRAPH_WIDTH / 2, 0);            //оси координат
-        graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2, 0, DEFAULT_GRAPH_WIDTH / 2 - 4, 10);
-        graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2, 0, DEFAULT_GRAPH_WIDTH / 2 + 4, 10);
-        graphic.drawLine(0, DEFAULT_GRAPH_HEIGHT / 2, DEFAULT_GRAPH_WIDTH, DEFAULT_GRAPH_HEIGHT / 2);
+        graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2, DEFAULT_GRAPH_HEIGHT, DEFAULT_GRAPH_WIDTH / 2, 0); // Oy
+        graphic.drawLine(0, DEFAULT_GRAPH_HEIGHT / 2, DEFAULT_GRAPH_WIDTH, DEFAULT_GRAPH_HEIGHT / 2); // Ox
+        // стрелки у Ox
         graphic.drawLine(DEFAULT_GRAPH_WIDTH - 10, DEFAULT_GRAPH_HEIGHT / 2 - 4, DEFAULT_GRAPH_WIDTH, DEFAULT_GRAPH_HEIGHT / 2);
         graphic.drawLine(DEFAULT_GRAPH_WIDTH - 10, DEFAULT_GRAPH_HEIGHT / 2 + 4, DEFAULT_GRAPH_WIDTH, DEFAULT_GRAPH_HEIGHT / 2);
+        // стрелки у Oy
+        graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2, 0, DEFAULT_GRAPH_WIDTH / 2 - 4, 10);
+        graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2, 0, DEFAULT_GRAPH_WIDTH / 2 + 4, 10);
 
         graphic.drawString("0", DEFAULT_GRAPH_WIDTH / 2 + 2, DEFAULT_GRAPH_HEIGHT / 2 + 14);
-        for (int i = radius; i >= 1; i--) {
-            graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2 - i * step, DEFAULT_GRAPH_HEIGHT / 2 - 3, DEFAULT_GRAPH_WIDTH / 2 - i * step, DEFAULT_GRAPH_HEIGHT / 2 + 3);
-            graphic.drawString("-" + i, DEFAULT_GRAPH_WIDTH / 2 - i * step, DEFAULT_GRAPH_HEIGHT / 2 + 17);
-            graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2 - 3, DEFAULT_GRAPH_HEIGHT / 2 - i * step, DEFAULT_GRAPH_WIDTH / 2 + 3, DEFAULT_GRAPH_HEIGHT / 2 - i * step);//цена деления
-            graphic.drawString("" + i, DEFAULT_GRAPH_WIDTH / 2 + 17, DEFAULT_GRAPH_HEIGHT / 2 - i * step + 5);
-            graphic.drawLine(DEFAULT_GRAPH_WIDTH - (DEFAULT_GRAPH_WIDTH / 2 - i * step), DEFAULT_GRAPH_HEIGHT / 2 - 3, DEFAULT_GRAPH_WIDTH - (DEFAULT_GRAPH_WIDTH / 2 - i * step), DEFAULT_GRAPH_HEIGHT / 2 + 3);            //цена деления
-            graphic.drawString("" + i, DEFAULT_GRAPH_WIDTH - (DEFAULT_GRAPH_WIDTH / 2 - i * step + 5), DEFAULT_GRAPH_HEIGHT / 2 + 17);
-            graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2 - 3, DEFAULT_GRAPH_HEIGHT - (DEFAULT_GRAPH_HEIGHT / 2 - step * i), DEFAULT_GRAPH_WIDTH / 2 + 3, DEFAULT_GRAPH_HEIGHT - (DEFAULT_GRAPH_HEIGHT / 2 - i * step));            //цена деления
-            graphic.drawString("-" + i, DEFAULT_GRAPH_WIDTH / 2 + 17, DEFAULT_GRAPH_HEIGHT - (DEFAULT_GRAPH_HEIGHT / 2 - i * step - 5));
+        for (int i = 10; i >= 1; i--) {
+            graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2 - i * 20, DEFAULT_GRAPH_HEIGHT / 2 - 3, DEFAULT_GRAPH_WIDTH / 2 - i * 20, DEFAULT_GRAPH_HEIGHT / 2 + 3);
+            graphic.drawString("-" + i, DEFAULT_GRAPH_WIDTH / 2 - i * 20, DEFAULT_GRAPH_HEIGHT / 2 + 17);
+            graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2 - 3, DEFAULT_GRAPH_HEIGHT / 2 - i * 20, DEFAULT_GRAPH_WIDTH / 2 + 3, DEFAULT_GRAPH_HEIGHT / 2 - i * 20);//цена деления
+            graphic.drawString("" + i, DEFAULT_GRAPH_WIDTH / 2 + 17, DEFAULT_GRAPH_HEIGHT / 2 - i * 20 + 5);
+            graphic.drawLine(DEFAULT_GRAPH_WIDTH - (DEFAULT_GRAPH_WIDTH / 2 - i * 20), DEFAULT_GRAPH_HEIGHT / 2 - 3, DEFAULT_GRAPH_WIDTH - (DEFAULT_GRAPH_WIDTH / 2 - i * 20), DEFAULT_GRAPH_HEIGHT / 2 + 3);            //цена деления
+            graphic.drawString("" + i, DEFAULT_GRAPH_WIDTH - (DEFAULT_GRAPH_WIDTH / 2 - i * 20 + 5), DEFAULT_GRAPH_HEIGHT / 2 + 17);
+            graphic.drawLine(DEFAULT_GRAPH_WIDTH / 2 - 3, DEFAULT_GRAPH_HEIGHT - (DEFAULT_GRAPH_HEIGHT / 2 - 20 * i), DEFAULT_GRAPH_WIDTH / 2 + 3, DEFAULT_GRAPH_HEIGHT - (DEFAULT_GRAPH_HEIGHT / 2 - i * 20));            //цена деления
+            graphic.drawString("-" + i, DEFAULT_GRAPH_WIDTH / 2 + 17, DEFAULT_GRAPH_HEIGHT - (DEFAULT_GRAPH_HEIGHT / 2 - i * 20 - 5));
         }
         double x1;
         double y1;
         if (flag) {
+            System.out.println(x + " " + y);
             x1 = (x - DEFAULT_GRAPH_WIDTH / 2) / stepPast;
             y1 = (-y + DEFAULT_GRAPH_HEIGHT / 2) / stepPast;
         } else {
@@ -104,10 +114,10 @@ public class Graph extends JPanel implements Runnable {
             y1 = y;
         }
         boolean fcolor;
-        if (((x1 <= radius & x1 >= 0) & (y1 <= radius & y1 >= 0) &
-                ((Math.pow(x1, 2) + Math.pow(y1, 2) <= (Math.pow(radius, 2)))) || // изменить условие для окружности
-                (x1 >= -radius & x1 <= 0 & y1 >= -radius & y1 <= 0) || // квадрат
-                ((x1 >= -(double) radius / 2.0) & (x1 <= 0) & (y1 <= (double) radius / 2.0) & (y1 >= 0) & (y1 <= x1 + (double) radius / 2.0)))) {
+        if (((x1 <= this.radius & x1 >= 0) & (y1 <= this.radius & y1 >= 0) &
+                ((Math.pow(x1, 2) + Math.pow(y1, 2) <= (Math.pow(this.radius, 2)))) || // изменить условие для окружности
+                (x1 >= -this.radius & x1 <= 0 & y1 >= -this.radius & y1 <= 0) || // квадрат
+                ((x1 >= -(double) this.radius / 2.0) & (x1 <= 0) & (y1 <= (double) this.radius / 2.0) & (y1 >= 0) & (y1 <= x1 + (double) this.radius / 2.0)))) {
             graphic.setColor(new Color(0x53F22C));
             green = 1;
             fcolor = red == 1;
@@ -179,4 +189,15 @@ public class Graph extends JPanel implements Runnable {
     private void begin() {
         new Thread(this).start();
     }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        this.setFlag(false);
+        if (!this.getFlag()) {
+            this.setY((int) getY());
+        }
+        this.repaint();
+    }
+
+
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,24 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.text.DecimalFormat;
+import javax.swing.border.Border;
 
-public class MainFrame implements ItemListener {
-    private Graph graph = new Graph();
+public class MainFrame extends JFrame implements ItemListener {
+    private GraphPanel graphPanel = new GraphPanel();
     private JLabel labelXData = new JLabel("x = ");
     private JLabel labelYData = new JLabel("y = ");
     private double yPoint;
-    private JSpinner spinner;
 
     MainFrame() {
-        JFrame mainFrame = new JFrame("Lab 4");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-
-        JPanel graphPanel = new JPanel();
-        graphPanel.setLayout(new BoxLayout(graphPanel, BoxLayout.Y_AXIS));
-        graphPanel.setPreferredSize(new Dimension(450, 250));
 
         JPanel dataPanel = new JPanel();
         dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
@@ -33,13 +28,14 @@ public class MainFrame implements ItemListener {
         comboBoxPanel.setLayout(new BoxLayout(comboBoxPanel, BoxLayout.Y_AXIS));
         comboBoxPanel.setPreferredSize(new Dimension(200, 100));
 
+        // add checkboxes to the panel
         for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
             checkBoxPanel.add(addYCoordinateOnPanel(i));
-        } // добавляем чекбоксы на панель
+        }
+        //  add a combobox to the panel
+        comboBoxPanel.add(addXCoordinateOnPanel());
 
-        comboBoxPanel.add(addXCoordinateOnPanel()); //  добавляем комбобоксы на панель
-
-        graph.addMouseListener(new mouseListener());
+        this.graphPanel.addMouseListener(new mouseListener());
 
         JLabel labelChoiceX = new JLabel("Choose the X-coordinate of a point:");
         labelChoiceX.add(Box.createVerticalStrut(300));
@@ -48,66 +44,60 @@ public class MainFrame implements ItemListener {
         dataPanel.add(labelChoiceX);
         dataPanel.add(comboBoxPanel);
 
-        // добавляем метки на панель данных
-
+        // add labels to the data panel
         dataPanel.add(new JLabel("Choose the Y-coordinate of a point:"));
         dataPanel.add(checkBoxPanel);
         dataPanel.add(new JLabel("Choose the value of a radius:"));
-        EventSpinner();
-        dataPanel.add(spinner);
         dataPanel.add(labelXData);
         dataPanel.add(labelYData);
-        // добавляем кнопку на панель данных
+        // add a button on the data panel
         Font font = new Font("Arial", Font.CENTER_BASELINE, 14);
         JButton button = new JButton("Add the point");
         button.setFont(font);
         button.setMargin(new Insets(30, 40, 30, 40));
         dataPanel.add(button);
-        // добавляем spinner на панель
+        // add spinner to the data panel
+        dataPanel.add(getSpinner());
+        // add the spacer on the data panel
+        dataPanel.add(Box.createVerticalStrut(100));
 
-        dataPanel.add(Box.createVerticalStrut(60));
-        ActionListener actionListener = new MainFrame.TestActionListener();
-        button.addActionListener(actionListener);
+        // CHANGE IT!!!!
+        // ActionListener actionListener = new GraphPanel().TestActionListener();
+        button.addActionListener(new GraphPanel());
 
-        graphPanel.add(graph);
-        // добавляем панели на главную панель
+        // add a panels to the main panel
         mainPanel.add(graphPanel, BorderLayout.EAST);
         mainPanel.add(dataPanel, BorderLayout.WEST);
-        /*
-        // рамка для отображения панелей
+        // add a borders for display panels
         Border etched = BorderFactory.createEtchedBorder(new Color(0xFF), new Color(0xFF719F));
-        // выставляем рамки
         labelChoiceX.setBorder(etched);
         comboBoxPanel.setBorder(etched);
         checkBoxPanel.setBorder(etched);
         dataPanel.setBorder(etched);
         mainPanel.setBorder(etched);
         graphPanel.setBorder(etched);
-        */
-        // Добавляем меню.
+        // add menu
         JMenu jMenu = new JMenu("About");
         JMenuItem menuItemAbout = new JMenuItem("Author");
         jMenu.add(menuItemAbout);
         JMenuBar jMenuBar = new JMenuBar();
         jMenuBar.add(jMenu);
-        mainFrame.setJMenuBar(jMenuBar);
-        // добавляем главную панель на окно
-        mainFrame.getContentPane().add(mainPanel);
-        mainFrame.setPreferredSize(new Dimension(710, 480));
-        mainFrame.setResizable(false);
-        mainFrame.pack();
-        mainFrame.setVisible(true);
-        mainFrame.setLocationRelativeTo(null);
+        this.setJMenuBar(jMenuBar);
+        // add the main panel on the frame
+        this.getContentPane().add(mainPanel);
+        this.setPreferredSize(new Dimension(730, 520));
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 
-
-    private JComboBox addXCoordinateOnPanel() {
+    private JComboBox<String> addXCoordinateOnPanel() {
         String[] dataCoordinates = new String[Data.getCountOfCoordinates()];
         for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
             dataCoordinates[i] = Double.toString(Data.getX(i));
-
         }
-        return new JComboBox(dataCoordinates);
+        return new JComboBox<>(dataCoordinates);
     }
 
     private JCheckBox addYCoordinateOnPanel(int i) {
@@ -120,16 +110,20 @@ public class MainFrame implements ItemListener {
         return checkBox;
     }
 
-    private void EventSpinner() {
+    private JSpinner getSpinner() {
         SpinnerModel spinnerModel = new SpinnerNumberModel(4, 1, 10, 1);
-        spinner = new JSpinner(spinnerModel);
-        // запрещаем редактировование spinner-а.
+        JSpinner spinner = new JSpinner(spinnerModel);
+        // forbid spinner editing.
         JTextField tempTextField = ((JSpinner.DefaultEditor) spinner.getEditor()).getTextField();
         tempTextField.setEditable(false);
         spinner.addChangeListener(event -> {
-            graph.setRadius((int) ((JSpinner) event.getSource()).getValue());
-            Paint();
+            graphPanel.setRadius((int) ((JSpinner) event.getSource()).getValue());
+            if (!graphPanel.getFlag()) {
+                graphPanel.setY((int) yPoint);
+            }
+            graphPanel.repaint();
         });
+        return spinner;
     }
 
     @Override
@@ -147,27 +141,20 @@ public class MainFrame implements ItemListener {
             }
     }
 
-    private void Paint() {
-        if (!graph.getFlag()) {
-            graph.setY((int) yPoint);
-        }
-        graph.repaint();
-    }
-
     private class mouseListener implements MouseListener {
         @Override
         public void mouseClicked(MouseEvent mouseEvent) {
-            Graph graph = (Graph) mouseEvent.getSource();
-            graph.setRed(0);
-            graph.setGreen(0);
-            graph.setX(mouseEvent.getX());
-            graph.setY(mouseEvent.getY());
-            graph.setFlag(true);
-            graph.repaint();
+            GraphPanel graphPanel = (GraphPanel) mouseEvent.getSource();
+            graphPanel.setRed(0);
+            graphPanel.setGreen(0);
+            graphPanel.setX(mouseEvent.getX());
+            graphPanel.setY(mouseEvent.getY());
+            graphPanel.setFlag(true);
+            graphPanel.repaint();
             String pattern = "##0.0";
             DecimalFormat decimalFormat = new DecimalFormat(pattern);
-            labelXData.setText("x = " + decimalFormat.format((graph.getXCoordinate() - graph.getGraphWidth() / 2) / graph.getStep()));
-            labelYData.setText("y = " + decimalFormat.format(-(graph.getYCoordinate() - graph.getGraphHeight() / 2) / graph.getStep()));
+            labelXData.setText("x = " + decimalFormat.format((graphPanel.getXCoordinate() - graphPanel.getGraphWidth() / 2) / graphPanel.getStep()));
+            labelYData.setText("y = " + decimalFormat.format(-(graphPanel.getYCoordinate() - graphPanel.getGraphHeight() / 2) / graphPanel.getStep()));
         }
 
         @Override
@@ -184,14 +171,6 @@ public class MainFrame implements ItemListener {
 
         @Override
         public void mousePressed(MouseEvent mouseEvent) {
-        }
-    }
-
-    private class TestActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent actionEvent) {
-            graph.setFlag(false);
-            Paint();
         }
     }
 }
