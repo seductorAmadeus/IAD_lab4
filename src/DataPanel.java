b98d557d5c28e541698386f78d5f373b8dbf5445
amadeus-tentatio <martinraila@mail.ru>
2016-11-27 17:26:41 +0300

    Delivers the panel of data in the  separate class

--- src/DataPanel.java
+++ src/DataPanel.java
@@ -1,5 +1,115 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class DataPanel extends JPanel {
+public class DataPanel extends JPanel implements ItemListener {
+    private JLabel labelXData = new JLabel("x = ");
+    private JLabel labelYData = new JLabel("y = ");
+    private GraphPanel graphPanel;
+    private double yPoint;
+
+    DataPanel() {
+        graphPanel = new GraphPanel();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setPreferredSize(new Dimension(250, 500));
+
+        JLabel labelChoiceX = new JLabel("Choose the X-coordinate of a point:");
+        labelChoiceX.add(Box.createVerticalStrut(300));
+        labelChoiceX.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        JPanel checkBoxPanel = new JPanel();
+        checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.Y_AXIS));
+        checkBoxPanel.setPreferredSize(new Dimension(400, 100));
+
+        // add checkboxes to the panel
+        for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
+            checkBoxPanel.add(addYCoordinateOnPanel(i));
+        }
+
+        JPanel comboBoxPanel = new JPanel();
+        comboBoxPanel.setLayout(new BoxLayout(comboBoxPanel, BoxLayout.Y_AXIS));
+        comboBoxPanel.setPreferredSize(new Dimension(200, 100));
+        //  add a combobox to the panel
+        comboBoxPanel.add(addXCoordinateOnPanel());
+
+        this.add(labelChoiceX);
+        this.add(comboBoxPanel);
+        this.add(new JLabel("Choose the Y-coordinate of a point:"));
+        this.add(checkBoxPanel);
+        this.add(new JLabel("Choose the value of a radius:"));
+        this.add(labelXData);
+        this.add(labelYData);
+        this.add(getSpinner());
+        Font font = new Font("Arial", Font.CENTER_BASELINE, 14);
+        JButton button = new JButton("Add the point");
+        button.setFont(font);
+        button.setMargin(new Insets(30, 40, 30, 40));
+        // CHANGE IT!!!!
+        // ActionListener actionListener = new GraphPanel().TestActionListener();
+        button.addActionListener(new GraphPanel());
+        // add the spacer on the data panel
+        this.add(Box.createVerticalStrut(20));
+        this.add(button);
+        // add the spacer on the data panel
+        this.add(Box.createVerticalStrut(100));
+    }
+
+    public void changeLabelX(String text) {
+        labelXData.setText(text);
+    }
+
+    public void changeLabelY(String text) {
+        labelYData.setText(text);
+    }
+
+    private JSpinner getSpinner() {
+        SpinnerModel spinnerModel = new SpinnerNumberModel(4, 1, 10, 1);
+        JSpinner spinner = new JSpinner(spinnerModel);
+        // forbid spinner editing.
+        JTextField tempTextField = ((JSpinner.DefaultEditor) spinner.getEditor()).getTextField();
+        tempTextField.setEditable(false);
+        spinner.addChangeListener(event -> {
+            graphPanel.setRadius((int) ((JSpinner) event.getSource()).getValue());
+            if (!graphPanel.getFlag()) {
+                graphPanel.setY((int) yPoint);
+            }
+            graphPanel.repaint();
+        });
+        return spinner;
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent itemEvent) {
+        JCheckBox checkBox = (JCheckBox) itemEvent.getItem();
+        if (checkBox.isSelected())
+            yPoint = Double.parseDouble(checkBox.getText());
+        for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
+            if (Data.getCheckBox(i) != checkBox)
+                Data.getCheckBox(i).setEnabled(false);
+        }
+        if (!checkBox.isSelected())
+            for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
+                Data.getCheckBox(i).setEnabled(true);
+            }
+    }
+
+    private JComboBox<String> addXCoordinateOnPanel() {
+        String[] dataCoordinates = new String[Data.getCountOfCoordinates()];
+        for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
+            dataCoordinates[i] = Double.toString(Data.getX(i));
+        }
+        return new JComboBox<>(dataCoordinates);
+    }
+
+    private JCheckBox addYCoordinateOnPanel(int i) {
+        String checkBoxName;
+        JCheckBox checkBox;
+        checkBoxName = Double.toString(Data.getY(i));
+        checkBox = new JCheckBox(checkBoxName);
+        checkBox.addItemListener(this);
+        Data.setCheckBox(i, checkBox);
+        return checkBox;
+    }
 
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,89 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.text.DecimalFormat;
 import javax.swing.border.Border;
 
-public class MainFrame extends JFrame implements ItemListener {
-    private GraphPanel graphPanel = new GraphPanel();
-    private JLabel labelXData = new JLabel("x = ");
-    private JLabel labelYData = new JLabel("y = ");
-    private double yPoint;
+public class MainFrame extends JFrame {
+    private DataPanel dataPanel;
+    private GraphPanel graphPanel;
 
     MainFrame() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-
-        JPanel dataPanel = new JPanel();
-        dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
-        dataPanel.setPreferredSize(new Dimension(250, 500));
-
-        JPanel checkBoxPanel = new JPanel();
-        checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.Y_AXIS));
-        checkBoxPanel.setPreferredSize(new Dimension(400, 100));
-
-        JPanel comboBoxPanel = new JPanel();
-        comboBoxPanel.setLayout(new BoxLayout(comboBoxPanel, BoxLayout.Y_AXIS));
-        comboBoxPanel.setPreferredSize(new Dimension(200, 100));
-
-        // add checkboxes to the panel
-        for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
-            checkBoxPanel.add(addYCoordinateOnPanel(i));
-        }
-        //  add a combobox to the panel
-        comboBoxPanel.add(addXCoordinateOnPanel());
-
-        this.graphPanel.addMouseListener(new mouseListener());
-
-        JLabel labelChoiceX = new JLabel("Choose the X-coordinate of a point:");
-        labelChoiceX.add(Box.createVerticalStrut(300));
-        labelChoiceX.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        dataPanel.add(labelChoiceX);
-        dataPanel.add(comboBoxPanel);
-
-        // add labels to the data panel
-        dataPanel.add(new JLabel("Choose the Y-coordinate of a point:"));
-        dataPanel.add(checkBoxPanel);
-        dataPanel.add(new JLabel("Choose the value of a radius:"));
-        dataPanel.add(labelXData);
-        dataPanel.add(labelYData);
-        // add a button on the data panel
-        Font font = new Font("Arial", Font.CENTER_BASELINE, 14);
-        JButton button = new JButton("Add the point");
-        button.setFont(font);
-        button.setMargin(new Insets(30, 40, 30, 40));
-        dataPanel.add(button);
-        // add spinner to the data panel
-        dataPanel.add(getSpinner());
-        // add the spacer on the data panel
-        dataPanel.add(Box.createVerticalStrut(100));
-
-        // CHANGE IT!!!!
-        // ActionListener actionListener = new GraphPanel().TestActionListener();
-        button.addActionListener(new GraphPanel());
-
         // add a panels to the main panel
+        graphPanel = new GraphPanel();
         mainPanel.add(graphPanel, BorderLayout.EAST);
+        dataPanel = new DataPanel();
         mainPanel.add(dataPanel, BorderLayout.WEST);
         // add a borders for display panels
         Border etched = BorderFactory.createEtchedBorder(new Color(0xFF), new Color(0xFF719F));
-        labelChoiceX.setBorder(etched);
-        comboBoxPanel.setBorder(etched);
-        checkBoxPanel.setBorder(etched);
         dataPanel.setBorder(etched);
         mainPanel.setBorder(etched);
         graphPanel.setBorder(etched);
         // add menu
-        JMenu jMenu = new JMenu("About");
-        JMenuItem menuItemAbout = new JMenuItem("Author");
-        jMenu.add(menuItemAbout);
-        JMenuBar jMenuBar = new JMenuBar();
-        jMenuBar.add(jMenu);
-        this.setJMenuBar(jMenuBar);
+        this.setJMenuBar(getjMenuBar());
         // add the main panel on the frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().add(mainPanel);
         this.setPreferredSize(new Dimension(730, 520));
         this.setResizable(false);
@@ -92,85 +31,12 @@ public class MainFrame extends JFrame implements ItemListener {
         this.setLocationRelativeTo(null);
     }
 
-    private JComboBox<String> addXCoordinateOnPanel() {
-        String[] dataCoordinates = new String[Data.getCountOfCoordinates()];
-        for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
-            dataCoordinates[i] = Double.toString(Data.getX(i));
-        }
-        return new JComboBox<>(dataCoordinates);
-    }
-
-    private JCheckBox addYCoordinateOnPanel(int i) {
-        String checkBoxName;
-        JCheckBox checkBox;
-        checkBoxName = Double.toString(Data.getY(i));
-        checkBox = new JCheckBox(checkBoxName);
-        checkBox.addItemListener(this);
-        Data.setCheckBox(i, checkBox);
-        return checkBox;
-    }
-
-    private JSpinner getSpinner() {
-        SpinnerModel spinnerModel = new SpinnerNumberModel(4, 1, 10, 1);
-        JSpinner spinner = new JSpinner(spinnerModel);
-        // forbid spinner editing.
-        JTextField tempTextField = ((JSpinner.DefaultEditor) spinner.getEditor()).getTextField();
-        tempTextField.setEditable(false);
-        spinner.addChangeListener(event -> {
-            graphPanel.setRadius((int) ((JSpinner) event.getSource()).getValue());
-            if (!graphPanel.getFlag()) {
-                graphPanel.setY((int) yPoint);
-            }
-            graphPanel.repaint();
-        });
-        return spinner;
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent itemEvent) {
-        JCheckBox checkBox = (JCheckBox) itemEvent.getItem();
-        if (checkBox.isSelected())
-            yPoint = Double.parseDouble(checkBox.getText());
-        for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
-            if (Data.getCheckBox(i) != checkBox)
-                Data.getCheckBox(i).setEnabled(false);
-        }
-        if (!checkBox.isSelected())
-            for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
-                Data.getCheckBox(i).setEnabled(true);
-            }
-    }
-
-    private class mouseListener implements MouseListener {
-        @Override
-        public void mouseClicked(MouseEvent mouseEvent) {
-            GraphPanel graphPanel = (GraphPanel) mouseEvent.getSource();
-            graphPanel.setRed(0);
-            graphPanel.setGreen(0);
-            graphPanel.setX(mouseEvent.getX());
-            graphPanel.setY(mouseEvent.getY());
-            graphPanel.setFlag(true);
-            graphPanel.repaint();
-            String pattern = "##0.0";
-            DecimalFormat decimalFormat = new DecimalFormat(pattern);
-            labelXData.setText("x = " + decimalFormat.format((graphPanel.getXCoordinate() - graphPanel.getGraphWidth() / 2) / graphPanel.getStep()));
-            labelYData.setText("y = " + decimalFormat.format(-(graphPanel.getYCoordinate() - graphPanel.getGraphHeight() / 2) / graphPanel.getStep()));
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent mouseEvent) {
-        }
-
-        @Override
-        public void mouseExited(MouseEvent mouseEvent) {
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent mouseEvent) {
-        }
-
-        @Override
-        public void mousePressed(MouseEvent mouseEvent) {
-        }
+    private JMenuBar getjMenuBar() {
+        JMenu jMenu = new JMenu("About");
+        JMenuItem menuItemAbout = new JMenuItem("Author");
+        jMenu.add(menuItemAbout);
+        JMenuBar jMenuBar = new JMenuBar();
+        jMenuBar.add(jMenu);
+        return jMenuBar;
     }
 }
