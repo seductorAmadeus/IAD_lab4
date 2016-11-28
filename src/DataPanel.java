53975bf98f4c1fd574a73440e4df41a4e2296849
amadeus-tentatio <martinraila@mail.ru>
2016-11-28 07:21:16 +0300

    Test only: added option to mark the x coordinate

--- src/DataPanel.java
+++ src/DataPanel.java
@@ -1,16 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
+import java.text.DecimalFormat;
 
-public class DataPanel extends JPanel implements ItemListener {
+public class DataPanel extends JPanel implements ItemListener, ActionListener {
     private JLabel labelXData = new JLabel("x = ");
     private JLabel labelYData = new JLabel("y = ");
     private GraphPanel graphPanel;
     private double yPoint;
+    private double xPoint;
+    private DecimalFormat decimalFormat = new DecimalFormat("##0.0");
 
-    DataPanel() {
-        graphPanel = new GraphPanel();
+    DataPanel(GraphPanel graphPanel) {
+        this.graphPanel = graphPanel;
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setPreferredSize(new Dimension(230, 500));
 
@@ -31,7 +33,20 @@ public class DataPanel extends JPanel implements ItemListener {
         comboBoxPanel.setLayout(new BoxLayout(comboBoxPanel, BoxLayout.Y_AXIS));
         comboBoxPanel.setPreferredSize(new Dimension(200, 100));
         //  add a combobox to the panel
-        comboBoxPanel.add(addXCoordinateOnPanel());
+        ActionListener actionListenerComboBox = e -> {
+            JComboBox comboBox = (JComboBox) e.getSource();
+            String value = comboBox.getSelectedItem().toString();
+            comboBox.actionPerformed(e);
+            xPoint = Double.parseDouble(value);
+            //fix it!
+            this.changeLabelX("x = " + decimalFormat.format((graphPanel.getXCoordinate() - graphPanel.getGraphWidth() / 2) / graphPanel.getStep()));
+            comboBox.actionPerformed(e);
+        };
+
+        JComboBox<String> comboBox;
+        comboBox = addXCoordinateOnPanel();
+        comboBox.addActionListener(actionListenerComboBox);
+        comboBoxPanel.add(comboBox);
 
         this.add(labelChoiceX);
         this.add(comboBoxPanel);
@@ -47,7 +62,8 @@ public class DataPanel extends JPanel implements ItemListener {
         button.setMargin(new Insets(30, 40, 30, 40));
         // CHANGE IT!!!!
         // ActionListener actionListener = new GraphPanel().TestActionListener();
-        button.addActionListener(new GraphPanel());
+
+        button.addActionListener(this);
         // add the spacer on the data panel
         this.add(Box.createVerticalStrut(20));
         this.add(button);
@@ -82,8 +98,11 @@ public class DataPanel extends JPanel implements ItemListener {
     @Override
     public void itemStateChanged(ItemEvent itemEvent) {
         JCheckBox checkBox = (JCheckBox) itemEvent.getItem();
-        if (checkBox.isSelected())
+        if (checkBox.isSelected()) {
             yPoint = Double.parseDouble(checkBox.getText());
+            //fix it!
+            this.changeLabelY("y = " + decimalFormat.format(-(graphPanel.getYCoordinate() - graphPanel.getGraphHeight() / 2) / graphPanel.getStep()));
+        }
         for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
             if (Data.getCheckBox(i) != checkBox)
                 Data.getCheckBox(i).setEnabled(false);
@@ -95,21 +114,38 @@ public class DataPanel extends JPanel implements ItemListener {
     }
 
     private JComboBox<String> addXCoordinateOnPanel() {
-        String[] dataCoordinates = new String[Data.getCountOfCoordinates()];
+        String[] dataCoordinates = new String[Data.getCountOfCoordinates() + 1];
         for (int i = 0; i < Data.getCountOfCoordinates(); i++) {
             dataCoordinates[i] = Double.toString(Data.getX(i));
         }
-        return new JComboBox<>(dataCoordinates);
+        dataCoordinates[Data.getCountOfCoordinates()] = "";
+        JComboBox<String> jComboBox = new JComboBox<>(dataCoordinates);
+        jComboBox.setSelectedIndex(Data.getCountOfCoordinates());
+        return jComboBox;
     }
+
 
     private JCheckBox addYCoordinateOnPanel(int i) {
         String checkBoxName;
-        JCheckBox checkBox;
         checkBoxName = Double.toString(Data.getY(i));
-        checkBox = new JCheckBox(checkBoxName);
+        JCheckBox checkBox = new JCheckBox(checkBoxName);
         checkBox.addItemListener(this);
         Data.setCheckBox(i, checkBox);
         return checkBox;
+    }
+
+    private void Paint(GraphPanel graphPanel) {
+        if (!graphPanel.getFlag()) {
+            graphPanel.setY((int) yPoint);
+            graphPanel.setX((int) xPoint);
+        }
+        graphPanel.repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        graphPanel.setFlag(false);
+        Paint(graphPanel);
     }
 
 }
